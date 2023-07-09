a850bef4c14cdd97524fd708aa1a76d21b22b3ed
Yookyubin <binde73@naver.com>
2023-07-09 18:54:40 +0900

    input과 timer

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -9,14 +9,6 @@ void Engine::Init(const WindowInfo& info)
 	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
 	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
 
-	_device = make_shared<Device>();
-	_cmdQueue = make_shared<CommandQueue>();
-	_swapChain = make_shared<SwapChain>();
-	_rootSignature = make_shared<RootSignature>();
-	_cb = make_shared<ConstantBuffer>();
-	_tableDescHeap = make_shared<TableDescriptorHeap>();
-	_depthStencilBuffer = make_shared<DepthStencilBuffer>();
-
 	// 각 클래스들의 Init() 함수 호출
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
@@ -25,6 +17,9 @@ void Engine::Init(const WindowInfo& info)
 	_cb->Init(sizeof(Transform), 256); // hlsli의 cbufffer Test_B0 혹은 Test_B1의 크기에 맞춰준다.
 	_tableDescHeap->Init(256);
 	_depthStencilBuffer->Init(_window);
+
+	_input->Init(info.hwnd);
+	_timer->Init();
 
 	ResizeWindow(_window.width, _window.height);
 }
@@ -36,6 +31,14 @@ void Engine::Render()
 	// TODO : 나머지 물체를 그려준다.
 
 	RenderEnd();
+}
+
+void Engine::Update()
+{
+	_input->Update();
+	_timer->Update();
+
+	ShowFps();
 }
 
 void Engine::RenderBegin()
@@ -58,4 +61,14 @@ void Engine::ResizeWindow(int32 width, int32 height)
 	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
 
 	_depthStencilBuffer->Init(_window);
+}
+
+void Engine::ShowFps()
+{
+	uint32 fps = _timer->GetFps();
+
+	WCHAR text[100] = L"";
+	::wsprintf(text, L"FPS : %d", fps);
+
+	::SetWindowText(_window.hwnd, text);
 }
